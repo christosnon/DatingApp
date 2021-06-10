2faf92b608ea64fd1913e0d34b160bae4d103454
christosnon <christosnonis@gmail.com>
2021-06-10 16:41:08 +0300

    End of 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,19 +7,23 @@ using System.Text;
 using API.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
+using API.Interfaces;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
             _context = context;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
 
 
@@ -42,12 +46,16 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto 
+            {
+                    Username = user.UserName,
+                    Token = _tokenService.CreateToken(user)
+            };
 
         }
     
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
@@ -62,7 +70,11 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
 
-            return user;
+            return new UserDto 
+            {
+                    Username = user.UserName,
+                    Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
