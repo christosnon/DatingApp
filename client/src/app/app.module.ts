f17e2f376cbc4975dcf835c00d88bfae61ee7410
christos-nonis <christos@aylonvalue.com>
2021-07-05 16:30:37 +0300

    Start of 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
