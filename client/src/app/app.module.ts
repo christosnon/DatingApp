e5336995887797c77d2db1870b7ec1738637a3bd
christos-nonis <christos@aylonvalue.com>
2021-07-07 17:03:00 +0300

    New Components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 
@@ -21,7 +25,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
     ],
   imports: [
     BrowserModule,
