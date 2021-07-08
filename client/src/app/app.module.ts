966b3f1bf1718221d12c16a71d913f96f29ec2f7
christos-nonis <christos@aylonvalue.com>
2021-07-08 10:59:52 +0300

    Fixed login issue

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -37,8 +38,10 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     NgbModule,
     FormsModule,
-    BsDropdownModule.forRoot()
-
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
